168ef4e151781edbd59e052e36ffd1c52149e5ee
Thoai <lightsword2404@gmail.com>
2019-05-05 18:47:28 +0700

    commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 1612664-1612669_DAGK
 Xây dựng ứng dụng Báo Điện Tử TH16-News front-end
+Github: https://github.com/thinhlu123/1612664-1612669_DAGK
+Nhóm em có dẫn link online của 5 bức hình trong "thi-tran-kim-cuong.html" để giảm bớt dung lượng hình ảnh.
--- ckeditor/build-config.js
+++ ckeditor/build-config.js
@@ -13,10 +13,10 @@
  * (1) https://ckeditor.com/cke4/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) https://ckeditor.com/cke4/builder/8ff7b71bcccc296bcc3a4a5b82942d17
+ * (2) https://ckeditor.com/cke4/builder/385c76873039eb283de3c5dfbd907400
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) https://ckeditor.com/cke4/builder/download/8ff7b71bcccc296bcc3a4a5b82942d17
+ * (3) https://ckeditor.com/cke4/builder/download/385c76873039eb283de3c5dfbd907400
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -26,7 +26,7 @@
 
 var CKBUILDER_CONFIG = {
 	skin: 'moono-lisa',
-	preset: 'standard',
+	preset: 'basic',
 	ignore: [
 		'.DS_Store',
 		'.bender',
@@ -52,43 +52,18 @@ var CKBUILDER_CONFIG = {
 		'tests'
 	],
 	plugins : {
-		'a11yhelp' : 1,
 		'about' : 1,
 		'basicstyles' : 1,
-		'blockquote' : 1,
 		'clipboard' : 1,
-		'contextmenu' : 1,
-		'elementspath' : 1,
+		'easyimage' : 1,
 		'enterkey' : 1,
 		'entities' : 1,
-		'filebrowser' : 1,
 		'floatingspace' : 1,
-		'format' : 1,
-		'horizontalrule' : 1,
-		'htmlwriter' : 1,
-		'image' : 1,
 		'indentlist' : 1,
 		'link' : 1,
 		'list' : 1,
-		'magicline' : 1,
-		'maximize' : 1,
-		'pastefromword' : 1,
-		'pastetext' : 1,
-		'removeformat' : 1,
-		'resize' : 1,
-		'scayt' : 1,
-		'showborders' : 1,
-		'sourcearea' : 1,
-		'specialchar' : 1,
-		'stylescombo' : 1,
-		'tab' : 1,
-		'table' : 1,
-		'tableselection' : 1,
-		'tabletools' : 1,
 		'toolbar' : 1,
 		'undo' : 1,
-		'uploadimage' : 1,
-		'wsc' : 1,
 		'wysiwygarea' : 1
 	},
 	languages : {
